0fea5ad34b2296b3afbb93d512a4bf9db71fd2c7
marwan <marwan.benali@student.vinci.be>
2023-11-16 19:09:48 +0100

    add: new branch for ex3

--- __tests__/helloWorld.js
+++ __tests__/helloWorld.js
@@ -5,21 +5,21 @@ test('bob should return "Hello, Bob."', () => {
   expect(greet('Bob')).toBe('Hello, Bob.');
 });
 
-//exe2 
+// exe2
 
-//test 1
+// test 1
 test('null should return Hello, my friend.', () => {
   expect(greet(null)).toBe('Hello, my friend.');
 });
 
-//test 2
+// test 2
 
 test('undefined name should return Hello, my friend.', () => {
   expect(greet(undefined)).toBe('Hello, my friend.');
 });
 
-//test 3
+// test 3
 
 test('empty name should return Hello, my friend.', () => {
-  expect(greet("")).toBe('Hello, my friend.');
+  expect(greet('')).toBe('Hello, my friend.');
 });
--- src/app.js
+++ src/app.js
@@ -1,17 +1,15 @@
 function greet (name) {
-  if (isUndefinedOrNull(name)) return "Hello, my friend.";
-  if (isEmptyString(name)) return "Hello, my friend.";
+  if (isUndefinedOrNull(name)) return 'Hello, my friend.';
+  if (isEmptyString(name)) return 'Hello, my friend.';
   return 'Hello, ' + name + '.';
 }
 
-
-
-  function isUndefinedOrNull(name) {
-    return name === undefined || name === null;
+function isUndefinedOrNull (name) {
+  return name === undefined || name === null;
 }
 
-function isEmptyString(name) {
-    return name === "";
+function isEmptyString (name) {
+  return name === '';
 }
 
 module.exports = greet;
