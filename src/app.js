93284f5edbb72a2555fa5563ce22096fdd851060
Resul <resulramadan@icloud.com>
2023-11-16 18:56:02 +0100

    refractor: create fucntion for null, undefined and empty case

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 function greet (name) {
-  if (name === null) return "Hello, my friend.";
-  if (name === undefined) return "Hello, my friend.";
+  if (isUndefinedOrNull(name)) return "Hello, my friend.";
+  if (isEmptyString(name)) return "Hello, my friend.";
   return 'Hello, ' + name + '.';
+}
+
+
+
+  function isUndefinedOrNull(name) {
+    return name === undefined || name === null;
+}
+
+function isEmptyString(name) {
+    return name === "";
 }
 
 module.exports = greet;
